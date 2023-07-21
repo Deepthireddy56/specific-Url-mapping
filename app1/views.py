b208944d8ca5f079a6e1b1f7b008ef82b721f681
Deepthireddy56 <bobbadeepthi86@gmail.com>
2023-07-21 23:09:17 +0530

    first commit

--- app1/views.py
+++ app1/views.py
@@ -2,6 +2,6 @@ from django.shortcuts import render
 from django.http import HttpResponse
 # Create your views here.
 def first(request):
-    return HttpResponse('<h1>Python Full Stack</h1')
+    return HttpResponse('<h1>Python Full Stack</h1>')
 def second(request):
-    return HttpResponse('<h1>Django Framework</h1')
+    return HttpResponse('<h1>Django Framework</h1>')
